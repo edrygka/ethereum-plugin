1ccd63da79935f3589a98fbdbf745c016ecd60ab
Eduard <edrygha@gmail.com>
2018-08-08 16:28:57 +0300

    fixing, getBalance functionality, cors setup

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 /* Config file */
 
 module.exports = {
+    domenTest : "127.0.0.1",
+    domenProd : "206.189.12.190",
     port : 3000,
     urlProvider : "https://ropsten.infura.io",
     urlGasPrice : "https://ethgasstation.info/json/ethgasAPI.json"
--- routes.js
+++ routes.js
@@ -21,40 +21,51 @@ const getCurrentGasPrices = () => {
 }
 
 module.exports = app => {
-    app.post('/sendTransaction', async (req, res, next) => {
-        const sender = req.body.from
+    app.post('/sendTransaction', (req, res, next) => {
         const privKey = req.body.privKey
         const reciver = req.body.reciver
-        const amount = req.body.amount
-        const gasprice = req.body.gasprice
-        const nonce = req.body.nonce
+        const amount = Number(req.body.amount)
+        const gasprice = Number(req.body.gasprice)
+        const nonce = Number(req.body.nonce)
+
         const details = {
             "to": reciver,
-            "value": web3.utils.toHex(web3.utils.toWei(amount, 'ether') ),
+            "value": amount * 10e18,
             "gas": 21000,
-            "gasPrice": gasprice * 1000000000, // converts the gwei price to wei(*10^9)
+            "gasPrice": gasprice * 10e9, // converts the gwei price to wei(*10^9)
             "nonce": nonce,
             "chainId": 3 // EIP 155 chainId - mainnet: 1, rinkeby: 4, ropsten: 3, morden: 2
         }
         
         try {
-
             const transaction = new EthereumTx(details)
-
             transaction.sign(Buffer.from(privKey, 'hex'))
-  
             const serializedTransaction = transaction.serialize()
-
-            web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'), (err, result) => {
-                if(err) throw err
+            web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'))
+            .on('transactionHash', result => {
                 logger.info('Transaction successfuly performed ' + result)
                 res.send({code: 0, data: result})
             })
-
+            .on('confirmation', result => {
+                logger.info('Transaction has already ' + result + ' confirmations')
+                // if(result == 5){
+                //     res.send({code: 0, data: "Transaction has already " + result + " confirmations"})
+                // }
+            })
+            // TODO: Error handler 
+            .on('error', error => {
+                next(error)
+            })
         } catch(e){
             logger.error(e.message)
-            res.send({code: 1, data: e.message})
+            next(e)
         }
+        
+    })
+
+    app.post('/sendTransaction', (err, req, res) => { 
+        console.log("Hello ======================== " + err)
+        res.send({code: 1, data: err.message})
     })
 
     app.get('/getBalance', (req, res) => {
